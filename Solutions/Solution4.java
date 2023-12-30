27942f8a0dab5e8ca51acf6474f491429b15e959
Berkin Öztürk <berkin.ozturk@std.izmirekonomi.edu.tr>
2023-12-30 17:45:36 +0300

    day#5

--- Solutions/Solution4.java
+++ Solutions/Solution4.java
@@ -26,7 +26,7 @@ class Result {
 
 }
 
-public class Solutions4 {
+public class Solution4 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
